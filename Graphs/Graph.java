a55f14ecf207aad6b497c832d1d619fb0e8ab907
Neeraj <nexengineer@gmail.com>
2022-07-31 01:22:31 +0530

    Adding graph implementation

---
+++ Graphs/Graph.java
@@ -0,0 +1,105 @@
+package Graphs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+public class Graph {
+    private Map<Integer, ArrayList<Integer>> adjencyList;
+
+    Graph () {
+        adjencyList = new HashMap<>();
+    }
+
+    public void addNode(int value) {
+        adjencyList.putIfAbsent(value, new ArrayList<>());
+    }
+
+    public void removeNode(int value, boolean isBiDirectional) {
+        if (adjencyList.get(value) != null){
+            if (isBiDirectional){
+                adjencyList.values().stream().forEach(e -> e.remove(value));
+            }
+            adjencyList.get(value).clear();
+            adjencyList.remove(value);
+        }
+    }
+
+    public void addEdges(int source, int destination, boolean isBiDirectional) {
+        if (adjencyList.get(source) == null || adjencyList.get(destination) == null){
+            System.out.println("Source or Destination is not present in the graph");
+            return;
+        }
+
+        adjencyList.get(source).add(destination);
+        if(isBiDirectional){
+            adjencyList.get(destination).add(source);
+        }
+    }
+
+    public void removeEdge(int source, int destination, boolean isBiDirectional){
+        if(adjencyList.get(source) == null || adjencyList.get(destination) == null){
+            System.out.println("Source or destination is not present in the graph");
+            return;
+        }
+        if(isBiDirectional){
+            adjencyList.get(destination).remove(source);
+        }
+        adjencyList.get(source).remove(source);
+    }
+
+    public void printGraph(){
+        Set<Integer> allKeys = adjencyList.keySet();
+        allKeys.stream().forEach(obj -> System.out.println("Node: " + obj + " AdjencyList: " + adjencyList.get(obj)));
+    }
+
+    public void doDFSRecursive(int at, List<Integer> visited) {
+        if (visited.contains(at)){
+            return;
+        }
+        visited.add(at);
+        System.out.println(at);
+        adjencyList.get(at).stream().forEach(e -> doDFSRecursive(e, visited));
+    }
+
+    public void doDFSIterative(int at){
+        List<Integer> visited = new ArrayList<Integer>();
+        Stack<Integer> history = new Stack<>();
+        history.push(at);
+        while(!history.isEmpty()){
+            int node = history.pop();
+            visited.add(node);
+            // System.out.println(node);
+            System.out.println(visited);
+            adjencyList.get(node).forEach(e -> {
+                if(!visited.contains(e))
+                    history.push(e);
+            });
+            // System.out.println(history);
+        }
+    }
+
+    public void doBFS(int start){
+        Set<Integer> keys = adjencyList.keySet();
+        boolean visited[] = new boolean[keys.size() + 1];
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        visited[start] = true;
+        queue.add(start);
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            System.out.println(node);
+            adjencyList.get(node).stream().forEach(e -> {
+                if(!visited[e]){
+                    visited[e] = true;
+                    queue.add(e);
+                }
+            });
+        }
+    }
+}
---
+++ Graphs/GraphRunner.java
@@ -0,0 +1,38 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class GraphRunner {
+    public static void main(String [] args){
+        runGraphSimpleGraph();
+    }
+
+    public static void runGraphSimpleGraph(){
+        System.out.println("Inside Run Simple Graph");
+        System.out.println("------------------------------");
+        Graph graph = new Graph();
+        graph.addNode(1);
+        graph.addNode(2);
+        graph.addNode(3);
+        graph.addNode(4);
+        graph.addNode(5);
+        graph.addNode(6);
+        graph.printGraph();
+
+        System.out.println("------------------------------");
+        graph.addEdges(1, 2, true);
+        graph.addEdges(1, 3, true);
+        graph.addEdges(3, 6, true);
+        graph.addEdges(3, 5, true);
+        graph.addEdges(5, 4, true);
+        graph.addEdges(6, 4, true);
+        graph.printGraph();
+
+        System.out.println("------------------------------");
+        graph.doDFSRecursive(1, new ArrayList<>());
+        System.out.println("------------------------------");
+        graph.doDFSIterative(1);
+        System.out.println("------------------------------");
+        graph.doBFS(1);
+    }
+}
