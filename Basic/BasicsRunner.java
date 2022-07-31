9ffbbd5c6f44adb68411520251db3feb81ae50a6
Neeraj <nexengineer@gmail.com>
2022-07-31 14:27:49 +0530

    #CTCI-1.2: Check if string is permutation of other string

--- Basic/BasicsRunner.java
+++ Basic/BasicsRunner.java
@@ -1,20 +1,28 @@
 import java.lang.String;
 
 import Strings.IsUnique;
+import Strings.Permutation;
 
 public class BasicsRunner {
     public static void main(String [] args) {
         System.out.println("This is the basic runner class");
-        checkUnique(); //<--------- check uniqueness of the string
+        // checkUnique(); //<--------- check uniqueness of the string
+        // checkPermutation(); //<-------- check if string is permutation of other string
     } 
 
     public static void checkUnique(){
-        System.out.println("------------------> Brute Force");
+        System.out.println("------------------> Brute Force -> O(n^2)");
         System.out.println(IsUnique.checkBruteForce("soome"));
         System.out.println(IsUnique.checkBruteForce("some"));
 
-        System.out.println("------------------> Optimized Approach");
+        System.out.println("------------------> Optimized Approach -> O(n)");
         System.out.println(IsUnique.check("some"));
         System.out.println(IsUnique.check("somee"));
+    }
+
+    public static void checkPermutation(){
+        System.out.println("------------------> Optimized Approach -> O(n)");
+        System.out.println(Permutation.checkForPermutation("sum", "mus"));
+        System.out.println(Permutation.checkForPermutation("mom", "mmom"));
     }
 }
---
+++ Basic/Strings/Permutation.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.Integer;
+
+public class Permutation {
+    public static boolean checkForPermutation(String first, String second){
+        Map<String, Integer> occurance = new HashMap<>();
+        for (int i=0; i< first.length(); i++){
+            occurance.merge(first.substring(i, i+1), 1, Integer::sum);
+        }
+
+        for (int i=0; i< second.length(); i++){
+            if(occurance.get(second.substring(i, i+1)) == null){
+                return false;
+            } else if(occurance.get(second.substring(i, i+1))<=0){
+                return false;
+            } else {
+                Integer value = occurance.get(second.substring(i, i+1));
+                occurance.put(second.substring(i, i+1), value-1);
+            }
+        }
+        return true;
+    }
+}
