35233ba313616677ab5c2ccc8e8d5bf2ae0df08e
Neeraj <nexengineer@gmail.com>
2022-08-01 11:16:32 +0530

    #CTCI-2: Replace occurance of nodes

---
+++ LinkedList/LinkListRunner.java
@@ -0,0 +1,40 @@
+import java.lang.String;
+public class LinkListRunner {
+    private static final String dashedLine = "------------------> ";
+
+    public static void main (String [] args){
+        System.out.println(dashedLine + "Linked List Runner");
+        // basicLinkedList();
+        replaceOccurances();
+    }
+
+    static void replaceOccurances(){
+        LinkedListNode ll = new LinkedListNode(1);
+        ll.addNode(2);
+        ll.addNode(3);
+        ll.addNode(1);
+        ll.addNode(5);
+        ll.addNode(2);
+        ll.addNode(7);
+        System.out.println(dashedLine);
+        LinkedListNode.print(ll);
+        LinkedListNode.removeDuplicateInLinkedList(ll);
+        System.out.println(dashedLine);
+        LinkedListNode.print(ll);
+    }
+
+    static void basicLinkedList(){
+        LinkedListNode ll = new LinkedListNode(1);
+        ll.addNode(2);
+        ll.addNode(3);
+        ll.addNode(4);
+        ll.addNode(5);
+        ll.addNode(6);
+        ll.addNode(7);
+        System.out.println(dashedLine);
+        LinkedListNode.print(ll);
+        ll.removeNode(5);
+        System.out.println(dashedLine);
+        LinkedListNode.print(ll);
+    }
+}
---
+++ LinkedList/LinkedListNode.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+// This the basic linked list representation
+public class LinkedListNode {
+    private LinkedListNode next;
+    private int data;
+
+    LinkedListNode(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public void addNode(int data){
+        LinkedListNode newNode =  new LinkedListNode(data);
+        LinkedListNode head = this;
+
+        while (head.next != null){
+            head = head.next;
+        }
+        head.next = newNode;
+    }
+
+    public void removeNode(int data){
+        LinkedListNode node = this;
+        if (node.data == data){
+            node = node.next;
+            return;
+        }
+        while (node.next != null){
+            if (node.next.data == data){
+                node.next = node.next.next;
+                return;
+            }
+            node = node.next;
+        }
+    }
+
+    public static void print(LinkedListNode head){
+        LinkedListNode node = head;
+        System.out.print(node.data + "->");
+        while(node.next != null){
+            node = node.next;
+            System.out.print(node.data + "->");
+        }
+        System.out.println();
+    }
+
+    // [Easy] Stmt: Find the duplicate values in unsorted Linked List 
+    public static void removeDuplicateInLinkedList(LinkedListNode linkedList) {
+        Set<Integer>track = new HashSet<>();
+
+        LinkedListNode head = linkedList;
+        LinkedListNode previous  = null;
+        while(head.next != null){
+            if (track.contains(head.data)){
+                previous.next = head.next;
+            } else {
+                track.add(head.data);
+                previous = head;
+            }
+            head = head.next;
+        }
+    }
+
+    
+}
