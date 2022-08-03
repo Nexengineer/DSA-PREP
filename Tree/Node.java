91e0f92ce97cf9feb0c45337a4aa24c8cf5f90a7
Neeraj <nexengineer@gmail.com>
2022-08-03 10:23:14 +0530

    #CTCI-MISC: Adding node structure for tree

---
+++ Tree/Node.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class Node {
+    Integer data;
+    Node leftChild;
+    Node rightChild;
+
+    Node(Integer data){
+        this.data = data;
+        this.leftChild= null;
+        this.rightChild = null;
+    }
+}
