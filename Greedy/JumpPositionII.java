512d92aae947ff7b95f19a25af6cf26a7d797011
Neeraj <nexengineer@gmail.com>
2022-08-20 11:05:27 +0530

    #CTCI- Jump Game II

---
+++ Greedy/JumpPositionII.java
@@ -0,0 +1,17 @@
+package Greedy;
+
+// [Medium]: Jump Game II => https://leetcode.com/problems/jump-game-ii/
+public class JumpPositionII {
+    public int jump(int[] nums) {        
+        int res = 0, r = 0, l = 0, fur = 0;
+
+        while (r < nums.length - 1) {
+            fur = 0;
+            for (int i = l; i <= r; i++) fur = Math.max(fur, i + nums[i]);
+            l = r + 1;
+            r = fur;
+            res++;
+        }
+        return res;
+    }
+}
