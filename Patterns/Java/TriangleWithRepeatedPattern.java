ad821142dafdc209efe124c6c49bea77691b39da
Utkarsh Katiyar <98865912+utkarshkatiyar1@users.noreply.github.com>
2022-10-03 10:41:41 +0530

    Update TriangleWithRepeatedPattern.java

--- Patterns/Java/TriangleWithRepeatedPattern.java
+++ Patterns/Java/TriangleWithRepeatedPattern.java
@@ -1,3 +1,25 @@
+/* Triangle with Repeating Pattern */
+
+// Pattern:
+
+/*1
+
+1 2 1
+
+1 2 3 2 1
+
+1 2 3 4 3 2 1
+
+1 2 3 4 5 4 3 2 1
+
+1 2 3 4 5 6 5 4 3 2 1
+
+1 2 3 4 5 6 7 6 5 4 3 2 1*/
+
+
+
+
+
 import java.util.Scanner;
 
 public class MainClass
